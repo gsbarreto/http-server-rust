649526de3190c144db040a1708deda0f384625bf
Gabriel Barreto <gsbarretobr@gmail.com>
2024-05-01 16:57:42 -0300

    post a file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
-use std::{fs::File, io::{Read, Write}, net::{TcpListener, TcpStream}, thread, env};
+use std::{collections::HashMap, env, fs::File, io::{Read, Write}, net::{TcpListener, TcpStream}, thread};
+
+use itertools::Itertools;
 
 fn handle_request(mut stream: TcpStream) {
-    let mut buffer = [0;1024];
-    stream.read(&mut buffer).unwrap();
-    let request = String::from_utf8_lossy(&buffer).to_string();
-    let route = request.split_whitespace().collect::<Vec<&str>>()[1];
-    
-    let splitted_route = route.split('/').collect::<Vec<&str>>();
-    let first_param = splitted_route[1];
-
     let args: Vec<String> = env::args().collect();
     let index_directory_arg = args.iter().position(|x| x.contains("--directory"));
     let mut directory_path = "/";
@@ -17,42 +11,93 @@ fn handle_request(mut stream: TcpStream) {
         None => {}
     }
 
-    match first_param {
-        "" => stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap(),
-        "files" => {
-            let filename = splitted_route[2];
-            if filename == ""  {
-                stream.write_all(b"HTTP/1.1 404 OK\r\n\r\n").unwrap();
-                return;
-            }
-            let file = File::open(format!("{directory_path}/{filename}"));
-            match file {
-                Ok(mut file) => {
-                    let mut contents = String::new();
-                    let _ = file.read_to_string(&mut contents);
-                    stream.write_all(format!("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n",contents.len(), contents).as_bytes()).unwrap();
-
-                },
-                Err(_) => {
-                    stream.write_all(b"HTTP/1.1 404 OK\r\n\r\n").unwrap();
-                }
-            }
-        },
-        "user-agent" => {
-            let user_agent_line = request.split("\r\n").filter(|&x| x.contains("User-Agent")).collect::<Vec<&str>>()[0];
-            let user_agent = user_agent_line.split(": ").collect::<Vec<&str>>()[1];                   
-            stream.write_all(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n",user_agent.len(), user_agent).as_bytes()).unwrap();
-        },
-        "echo" => {
-            if splitted_route.len() == 3 {
-                let echo = splitted_route[2];
-                stream.write_all(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n", echo.len(), echo).as_bytes()).unwrap();
-            } else {
-                stream.write_all(b"HTTP/1.1 404 OK\r\n\r\n").unwrap();
-            }
+    let mut buffer = [0;1024];
+    stream.read(&mut buffer).unwrap();
+    let http_request = String::from_utf8_lossy(&buffer).to_string();
+    let request_lines = http_request.split("\r\n").collect::<Vec<&str>>();
+    let main_header = request_lines[0].split_whitespace().collect::<Vec<&str>>();
+    let method = main_header[0];
+    let endpoint: &str = main_header[1];
+    let splitted_endpoint = endpoint.split('/').collect::<Vec<&str>>();
+    let first_param = splitted_endpoint[1];
+    
+    let headers = request_lines[1..].iter().map(|line| {
+        let splitted_headers = line.split(": ").collect::<Vec<&str>>();
+        if splitted_headers.len() != 2 {
+            return ("body".to_string(), splitted_headers[0].to_string().split("\0").join(""));
         }
-        _ => stream.write_all(b"HTTP/1.1 404 OK\r\n\r\n").unwrap()
+        Some((splitted_headers[0].to_string(), splitted_headers[1].to_string())).unwrap()
+    }).collect::<HashMap<_,_>>();  
+
+    let body = if method == "POST" {
+        Some(headers.get("body").unwrap())
+    } else {
+        None
+    };
+
+    match method {
+        "GET" => {
+            match first_param {
+                "" => stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap(),
+                "files" => {
+                    let filename = splitted_endpoint[2];
+                    if filename == ""  {
+                        stream.write_all(b"HTTP/1.1 404 OK\r\n\r\n").unwrap();
+                        return;
+                    }
+                    
+                    let file = File::open(format!("{directory_path}/{filename}"));
+                    match file {
+                        Ok(mut file) => {
+                            let mut contents = String::new();
+                            let _ = file.read_to_string(&mut contents);
+                            stream.write_all(format!("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n",contents.len(), contents).as_bytes()).unwrap();
+
+                        },
+                        Err(_) => {
+                            stream.write_all(b"HTTP/1.1 404 OK\r\n\r\n").unwrap();
+                        }
+                    }                    
+                },
+                "user-agent" => {
+                    let user_agent = headers.get("User-Agent").unwrap();             
+                    stream.write_all(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n",user_agent.len(), user_agent).as_bytes()).unwrap();
+                },
+                "echo" => {
+                    if splitted_endpoint.len() == 3 {
+                        let echo = splitted_endpoint[2];
+                        stream.write_all(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n", echo.len(), echo).as_bytes()).unwrap();
+                    } else {
+                        stream.write_all(b"HTTP/1.1 404 OK\r\n\r\n").unwrap();
+                    }
+                }
+                _ => stream.write_all(b"HTTP/1.1 404 OK\r\n\r\n").unwrap()
+            }
+        },
+        "POST" => {
+            match first_param {
+                "files" => {
+                    let filename = splitted_endpoint[2];
+                    if filename == ""  {
+                        stream.write_all(b"HTTP/1.1 404 OK\r\n\r\n").unwrap();
+                        return;
+                    }
+                    
+                    let mut file = File::create(format!("{directory_path}/{filename}")).unwrap();
+                    file.write_all(body.unwrap().as_bytes()).unwrap();
+                    file.flush().unwrap();
+                    stream.write_all(b"HTTP/1.1 201 OK\r\n\r\n").unwrap();               
+                }
+                _ => stream.write_all(b"HTTP/1.1 404 OK\r\n\r\n").unwrap()
+            }
+        },
+        _ => {
+            println!("Unknown request to {}", endpoint);
+            stream.write_all(b"HTTP/1.1 404 OK\r\n\r\n").unwrap()
+        }
     }
+    
+    
 }
 fn main() {
     println!("Logs from your program will appear here!");
