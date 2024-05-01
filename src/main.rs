02de9b9aa40a9472c3223263160887b97d086883
Gabriel Barreto <gsbarretobr@gmail.com>
2024-05-01 14:19:14 -0300

    respond with content

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,20 @@ fn main() {
             Ok(mut stream) => {
                 let mut buffer = [0;1024];
                 stream.read(&mut buffer).unwrap();
-                let route = std::str::from_utf8(&buffer[5..6]).unwrap();
-                if route == " " {
+                let request = String::from_utf8_lossy(&buffer).to_string();
+                let route = request.split_whitespace().collect::<Vec<&str>>()[1];
+                let splitted_route = route.split('/').collect::<Vec<&str>>();
+                let first_param = splitted_route[1];
+                if first_param == "" {
                     stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
-                } else {
-                    stream.write_all(b"HTTP/1.1 404 OK\r\n\r\n").unwrap();
+                    continue;
                 }
+                if splitted_route.len() == 3 && first_param == "echo" {
+                    let second_param = splitted_route[2];
+                    stream.write_all(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n",second_param.len(), second_param).as_bytes()).unwrap();
+                    continue;
+                }   
+                stream.write_all(b"HTTP/1.1 404 OK\r\n\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
