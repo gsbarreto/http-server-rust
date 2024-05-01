0de381e652fcda4a9fd2fb669ddbeddec3a740ca
Gabriel Barreto <gsbarretobr@gmail.com>
2024-05-01 14:33:35 -0300

    parse header

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,27 @@ fn main() {
                 stream.read(&mut buffer).unwrap();
                 let request = String::from_utf8_lossy(&buffer).to_string();
                 let route = request.split_whitespace().collect::<Vec<&str>>()[1];
+                
                 let splitted_route = route.split('/').collect::<Vec<&str>>();
                 let first_param = splitted_route[1];
-                if first_param == "" {
-                    stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
-                    continue;
+                match first_param {
+                    "" => stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap(),
+                    "user-agent" => {
+                        let user_agent_line = request.split("\r\n").filter(|&x| x.contains("User-Agent")).collect::<Vec<&str>>()[0];
+                        let user_agent = user_agent_line.split(": ").collect::<Vec<&str>>()[1];                   
+                        stream.write_all(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n",user_agent.len(), user_agent).as_bytes()).unwrap();
+                    },
+                    "echo" => {
+                        if splitted_route.len() == 3 {
+                            let echo = splitted_route[2];
+                            stream.write_all(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n", echo.len(), echo).as_bytes()).unwrap();
+                        } else {
+                            stream.write_all(b"HTTP/1.1 404 OK\r\n\r\n").unwrap();
+                        }
+                    }
+                    _ => stream.write_all(b"HTTP/1.1 404 OK\r\n\r\n").unwrap()
                 }
-                if splitted_route.len() == 3 && first_param == "echo" {
-                    let second_param = splitted_route[2];
-                    stream.write_all(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n",second_param.len(), second_param).as_bytes()).unwrap();
-                    continue;
-                }   
-                stream.write_all(b"HTTP/1.1 404 OK\r\n\r\n").unwrap();
+                continue;
             }
             Err(e) => {
                 println!("error: {}", e);
